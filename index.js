25b7b951660b6f71dfaaa74c56970f8fc3cd84b8
Fco. Javier Barragán Felices <fjbarragan@dialogotecnologico.com>
2021-10-23 19:31:44 +0200

    Recibir query paramétros query con GET

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 7001;
 
@@ -15,28 +16,21 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 3000
-    },
-    {
-      name: 'Product 3',
-      price: 100
-    },
-    {
-      name: 'Product 4',
-      price: 50
-    },
-    {
-      name: 'Product 5',
-      price: 19
-    }
-  ]);
+  const products = [];
+  const { limit } = req.query;
+  for (let index = 0; index < (limit || 10); index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    });
+  }
+  res.json(products);
+});
+
+// Endpoint de forma específica debe de ir antes que endp
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
 });
 
 app.get('/products/:id', (req, res) => {
@@ -95,147 +89,158 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
   });
 });
 
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parámetros');
+  }
+});
 
 // ENDPOINTS DE BÚSQUEDA DE PERSONAS
 //PARA PERSONAS (todas)
 // todas, apellido, nombre, cedula, rol
-app.get('/users', (req, res) => {
-  res.json([
-    {
-      id: '11.111.111',
-      lastname: 'Lombardi González',
-      name: 'Pedro Augusto',
-      rol: ['estudiante'],
-      avatar: 'File'
-    },
-    {
-      id: '11.111.112',
-      lastname: 'Lombardi Egurrola',
-      name: 'Pedro Jesús',
-      rol: ['representante', 'docente', 'administrativo'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.113',
-      lastname: 'González de Lombardi',
-      name: 'Eunice Margarita',
-      rol: ['representante', 'administrativo'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.114',
-      lastname: 'Duarte Arambule',
-      name: 'Alfonsina Rosario',
-      rol: ['administrativo'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.115',
-      lastname: 'Pachuca Duarte',
-      name: 'María Rosa',
-      rol: ['representante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.116',
-      lastname: 'Reyes de Molina',
-      name: 'Elisa Margarita',
-      rol: ['docente'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.117',
-      lastname: 'García Padrón',
-      name: 'Oswaldo José',
-      rol: ['apoyo'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.118',
-      lastname: 'Buitriado Paz',
-      name: 'Gilberto Alejandro',
-      rol: ['estudiante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.119',
-      lastname: 'Cancino Ferrer',
-      name: 'Gonzalo José',
-      rol: ['estudiante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.120',
-      lastname: 'Medina Lombardi',
-      name: 'Augusto Alejandro',
-      rol: ['estudiante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.121',
-      lastname: 'Medina Lombardi',
-      name: 'Isabel Josefina',
-      rol: ['estudiante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.122',
-      lastname: 'Perdomo Lombardi',
-      name: 'Mario Ramón',
-      rol: ['representante'],
-      photo: 'File'
-    }
-  ]);
-});
+// app.get('/users', (req, res) => {
+//   res.json([
+//     {
+//       id: '11.111.111',
+//       lastname: 'Lombardi González',
+//       name: 'Pedro Augusto',
+//       rol: ['estudiante'],
+//       avatar: 'File'
+//     },
+//     {
+//       id: '11.111.112',
+//       lastname: 'Lombardi Egurrola',
+//       name: 'Pedro Jesús',
+//       rol: ['representante', 'docente', 'administrativo'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.113',
+//       lastname: 'González de Lombardi',
+//       name: 'Eunice Margarita',
+//       rol: ['representante', 'administrativo'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.114',
+//       lastname: 'Duarte Arambule',
+//       name: 'Alfonsina Rosario',
+//       rol: ['administrativo'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.115',
+//       lastname: 'Pachuca Duarte',
+//       name: 'María Rosa',
+//       rol: ['representante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.116',
+//       lastname: 'Reyes de Molina',
+//       name: 'Elisa Margarita',
+//       rol: ['docente'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.117',
+//       lastname: 'García Padrón',
+//       name: 'Oswaldo José',
+//       rol: ['apoyo'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.118',
+//       lastname: 'Buitriado Paz',
+//       name: 'Gilberto Alejandro',
+//       rol: ['estudiante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.119',
+//       lastname: 'Cancino Ferrer',
+//       name: 'Gonzalo José',
+//       rol: ['estudiante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.120',
+//       lastname: 'Medina Lombardi',
+//       name: 'Augusto Alejandro',
+//       rol: ['estudiante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.121',
+//       lastname: 'Medina Lombardi',
+//       name: 'Isabel Josefina',
+//       rol: ['estudiante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.122',
+//       lastname: 'Perdomo Lombardi',
+//       name: 'Mario Ramón',
+//       rol: ['representante'],
+//       photo: 'File'
+//     }
+//   ]);
+// });
 
-//PARA PERSONAS (todas por Apellido)
-app.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  res.json([
-    {
-      id: '11.111.111',
-      lastname: 'Lombardi González',
-      name: 'Pedro Augusto',
-      rol: ['estudiante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.112',
-      lastname: 'Lombardi Egurrola',
-      name: 'Pedro Jesús',
-      rol: ['representante', 'docente', 'administrativo'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.113',
-      lastname: 'González de Lombardi',
-      name: 'Eunice Margarita',
-      rol: ['representante', 'administrativo'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.120',
-      lastname: 'Medina Lombardi',
-      name: 'Augusto Alejandro',
-      rol: ['estudiante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.121',
-      lastname: 'Medina Lombardi',
-      name: 'Isabel Josefina',
-      rol: ['estudiante'],
-      photo: 'File'
-    },
-    {
-      id: '11.111.122',
-      lastname: 'Perdomo Lombardi',
-      name: 'Mario Ramón',
-      rol: ['representante'],
-      photo: 'File'
-    }
-  ]);
-});
+// //PARA PERSONAS (todas por Apellido)
+// app.get('/users/:id', (req, res) => {
+//   const { id } = req.params;
+//   res.json([
+//     {
+//       id: '11.111.111',
+//       lastname: 'Lombardi González',
+//       name: 'Pedro Augusto',
+//       rol: ['estudiante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.112',
+//       lastname: 'Lombardi Egurrola',
+//       name: 'Pedro Jesús',
+//       rol: ['representante', 'docente', 'administrativo'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.113',
+//       lastname: 'González de Lombardi',
+//       name: 'Eunice Margarita',
+//       rol: ['representante', 'administrativo'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.120',
+//       lastname: 'Medina Lombardi',
+//       name: 'Augusto Alejandro',
+//       rol: ['estudiante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.121',
+//       lastname: 'Medina Lombardi',
+//       name: 'Isabel Josefina',
+//       rol: ['estudiante'],
+//       photo: 'File'
+//     },
+//     {
+//       id: '11.111.122',
+//       lastname: 'Perdomo Lombardi',
+//       name: 'Mario Ramón',
+//       rol: ['representante'],
+//       photo: 'File'
+//     }
+//   ]);
+// });
 
 app.listen(port, () => {
   console.log(`listening on at http://localhost:${port}`);
