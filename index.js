2ccd030c86a11a79e27f46f4b86e8a742b533ed2
Javi Felices <javifelices@gmail.com>
2021-10-26 20:13:10 +0200

    Añadir validación de datos con @hapi/boom

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const routerApi = require('./routes');
 
-const { logErrors, errorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = 7001;
@@ -20,6 +24,7 @@ routerApi(app);
 
 // Los middlewares siempre deben ir después del routing
 app.use(logErrors);
+app.use(boomErrorHandler);
 app.use(errorHandler);
 // Es importante el orden en que se coloquen, porque es el orden en el que se ejecutarán. En este caso el logErrors es el único con un next, por lo tanto, si se colocara el errorHandler antes, ahí terminaría el proceso.
 
--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 
 faker.locale = 'es';
 
@@ -15,7 +16,8 @@ class ProductsService {
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
-        image: faker.image.imageUrl()
+        image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean()
       });
     }
   }
@@ -38,28 +40,44 @@ class ProductsService {
   }
 
   async findOne(id) {
-    const name = this.getTotal();
-    return this.products.find((item) => item.id === id);
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw boom.notFound('product not found');
+    }
+    if (product.isBlock) {
+      throw boom.conflict('product is blocked');
+    }
+    return product;
   }
 
   async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
-      throw new Error('product not found');
-    } else {
-      const product = this.products[index];
-      this.products[index] = {
-        ...product,
-        ...changes
-      };
-      return this.products[index];
+      throw boom.notFound('product not found');
     }
+    const product = this.products[index];
+    if (product.isBlock) {
+      throw boom.conflict('product is blocked');
+    }
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
+    // else {
+    //   const product = this.products[index];
+    //   this.products[index] = {
+    //     ...product,
+    //     ...changes
+    //   };
+    //   return this.products[index];
+    // }
   }
 
   async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
-      throw new Error('product not found');
+      throw boom.notFound('product not found');
     }
     this.products.splice(index, 1);
     return { id };
